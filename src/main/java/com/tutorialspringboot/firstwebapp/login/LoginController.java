37bcf0277bcc4ef7614ef79ac31323b35963df43
Aniket Ghodake <ghodakeaniket27@gmail.com>
2024-08-28 10:32:37 +0530

    28th August 2024

--- src/main/java/com/tutorialspringboot/firstwebapp/login/LoginController.java
+++ src/main/java/com/tutorialspringboot/firstwebapp/login/LoginController.java
@@ -1,28 +1,27 @@
 package com.tutorialspringboot.firstwebapp.login;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class LoginController {
 	
-	//private Logger logger = LoggerFactory.getLogger(getClass());
 	
-	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		
-		
-		model.put("name", name);
-//		logger.debug("Request param is {} ", name);
-//		logger.info("I want this printed at info level");
-//		logger.info("I want this printed at warn level");
-//		System.out.println("Request param is " + name);// NOT RECOMMENDED FOR PROD CODE
-		
-		
+	
+	@RequestMapping(value="login", method = RequestMethod.GET)
+	public String gotoLoginPage() {
 		return "login";
 	}
+	
+	
+	@RequestMapping(value="login", method = RequestMethod.POST)
+	public String gotoWelcomePage() {
+		return "welcome";
+	}
+	
+	
+	
 }
