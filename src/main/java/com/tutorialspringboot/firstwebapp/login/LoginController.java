0b32fae2ba1c51b9b2b9061fee044feb6b125fb6
Aniket Ghodake <ghodakeaniket27@gmail.com>
2024-08-29 20:02:36 +0530

    29th August 2024

--- src/main/java/com/tutorialspringboot/firstwebapp/login/LoginController.java
+++ src/main/java/com/tutorialspringboot/firstwebapp/login/LoginController.java
@@ -2,15 +2,23 @@ package com.tutorialspringboot.firstwebapp.login;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 	
-	
-	
+	private AuthenticationService authenticationService;
+	 
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+
 	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
@@ -18,8 +26,20 @@ public class LoginController {
 	
 	
 	@RequestMapping(value="login", method = RequestMethod.POST)
-	public String gotoWelcomePage() {
-		return "welcome";
+	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+	
+		if(authenticationService.authenticate(name, password)) {
+			model.put("name", name);
+			
+			
+				
+			
+			return "welcome";
+		}
+		
+		model.put("errorMessage", "Invalid Credientials ! Please try again.");
+		return "login";
+		
 	}
 	
 	
